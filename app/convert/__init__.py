1ed854148c84fd4a82836e5c75a1a29a748dbcd6
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-07-26 00:30:29 -0500

    trying to move the convert function above because I'm getting a 500 error

--- app/convert/__init__.py
+++ app/convert/__init__.py
@@ -6,6 +6,15 @@ from readability import Document
 import azure.functions as func
 
 
+def convert_html_to_md(url):
+    response = requests.get(url)
+    doc = Document(response.text)
+
+    html = doc.summary()
+
+    return pypandoc.convert_text(html, 'md', format='html')
+
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -25,12 +34,3 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
-
-
-def convert_html_to_md(url):
-    response = requests.get(url)
-    doc = Document(response.text)
-
-    html = doc.summary()
-
-    return pypandoc.convert_text(html, 'md', format='html')
