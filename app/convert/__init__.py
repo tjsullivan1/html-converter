161558af15d05b24c1acfd115833127c477a62d4
Tim Sullivan <timothyj.sullivan1@gmail.com>
2020-07-27 23:37:32 -0500

    added tested function code

--- app/convert/__init__.py
+++ app/convert/__init__.py
@@ -6,6 +6,15 @@ from readability import Document
 import azure.functions as func
 
 
+def convert_html_to_md(url):
+    response = requests.get(url)
+    doc = Document(response.text)
+
+    html = doc.summary()
+
+    return pypandoc.convert_text(html, 'md', format='html')
+
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -19,18 +28,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             url = req_body.get('url')
 
     if url:
-        return func.HttpResponse(f"Hello, {url}. This HTTP triggered function executed successfully.")
+        md = convert_html_to_md(url)
+        return func.HttpResponse(f"Hello, {md}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
-
-
-def convert_html_to_md(url):
-    response = requests.get(url)
-    doc = Document(response.text)
-
-    html = doc.summary()
-
-    return pypandoc.convert_text(html, 'md', format='html')
